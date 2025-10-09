ce3f5805489ae6a8d3cbfbe8d56fb5407c489d85
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-09 19:46:36 +0530

    prakash-suresh conversation comitted first time

---
+++ 02-conversation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#prakash:: hi suresh, how are you:
+#suresh:: hi prakash, i am good, what's going on 
+#prakash:: by god grace everything fine suresh
+#suresh::ok prakash will meet on saturday
