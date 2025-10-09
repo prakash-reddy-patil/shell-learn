833b1da102cc50ddfe45cec19a1ea3f12d78ea53
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-09 21:26:16 +0530

    modified varible

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -6,7 +6,7 @@
 PERSON1=patil
 PERSON2=prakash
 
-echo "PERSON1:: hi PERSON2, how are you?"
-echo "PERSON2:: hi PERSON1, i am good,how about you,and what's going on" 
-echo "PERSON1:: great! by god grace everything fine PERSON2"
-echo "PERSON2::ok PERSON1, will meet you on saturday this weekend"
+echo "$PERSON1:: hi $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, i am good,how about you,and what's going on" 
+echo "$PERSON1:: great! by god grace everything fine $PERSON2"
+echo "$PERSON2::ok $PERSON1, will meet you on saturday this weekend"
