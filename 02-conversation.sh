55870fa77971e1b398eb05596b2aa6d27dd5762e
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-09 19:55:20 +0530

    praksh-suresh coversation comitted second time

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#prakash:: hi suresh, how are you:
-#suresh:: hi prakash, i am good, what's going on 
-#prakash:: by god grace everything fine suresh
-#suresh::ok prakash will meet on saturday
+prakash:: hi suresh, how are you?
+suresh:: hi prakash, i am good,how about you,and what's going on 
+prakash:: great! by god grace everything fine suresh
+suresh::ok prakash, will meet you on saturday this weekend
