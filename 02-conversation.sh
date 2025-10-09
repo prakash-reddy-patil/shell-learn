44fc6f6b6099bef58ffa68a2f22271f6ee6fd119
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-09 20:16:04 +0530

    converstion comitted for third time

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-prakash:: hi suresh, how are you?
-suresh:: hi prakash, i am good,how about you,and what's going on 
-prakash:: great! by god grace everything fine suresh
-suresh::ok prakash, will meet you on saturday this weekend
+echo "prakash:: hi suresh, how are you?"
+echo "suresh:: hi prakash, i am good,how about you,and what's going on" 
+echo "prakash:: great! by god grace everything fine suresh"
+echo "suresh::ok prakash, will meet you on saturday this weekend"
