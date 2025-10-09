bc355b2fac901cb8b5048fc4c787a50a0782850f
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-09 21:19:21 +0530

    varibles comitted

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-echo "prakash:: hi suresh, how are you?"
-echo "suresh:: hi prakash, i am good,how about you,and what's going on" 
-echo "prakash:: great! by god grace everything fine suresh"
-echo "suresh::ok prakash, will meet you on saturday this weekend"
+
+# no space before or next to equal (=)
+
+PERSON1=patil
+PERSON2=prakash
+
+echo "PERSON1:: hi PERSON2, how are you?"
+echo "PERSON2:: hi PERSON1, i am good,how about you,and what's going on" 
+echo "PERSON1:: great! by god grace everything fine PERSON2"
+echo "PERSON2::ok PERSON1, will meet you on saturday this weekend"
