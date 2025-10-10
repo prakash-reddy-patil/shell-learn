8456ee871858f911d01aca298f0ec739b6df2147
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-10 21:45:31 +0530

    file 03 variable is comitted

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#Arguments
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1:: hi $PERSON2, how are you?"
+echo "$PERSON2:: hi $PERSON1, i am good,how about you,and what's going on" 
+echo "$PERSON1:: great! by god grace everything fine $PERSON2"
+echo "$PERSON2::ok $PERSON1, will meet you on saturday this weekend"
