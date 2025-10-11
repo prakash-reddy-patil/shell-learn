3d7047e8723d26072c1dfc8b5bb9526687602c52
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 15:34:58 +0530

    file 8 is comitted

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -8,3 +8,11 @@ num2=200
 sum=$((num1+num2))
 
 echo "the addition of two numbers is:$sum"
+
+
+LEADERS=$(("praksh" "trump" "modi" "putin"))
+
+echo "All leaders are present in array is:$LEADERS[@]"
+echo "the first leader in array is:$LEADERS[0]"
+echo "the third leader in array is:$LEADERS[2]"
+
