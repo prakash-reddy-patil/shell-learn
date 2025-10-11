76fd66fa9a337510f81e0fafa04744faf691b935
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 13:52:19 +0530

    file are comitted

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -10,4 +10,7 @@ echo "the user who is running the script is:$USER"
 echo "current directoty os the script:$PWD"
 echo "home directory of user is:$HOME"
 echo "process instant id (PID) of the process is:$$"
+
+sleep 10 $    #processer sleeps in background for 10 sec,below varible $! gives which running background process id (PID)
+
 echo "PID of last command in back ground running is:$!"
