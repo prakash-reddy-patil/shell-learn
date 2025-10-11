4d666eb8a367f45b3e1e9b2c74ac816419b2a5e5
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 13:45:47 +0530

    file are comitted

--- 07-special-vars.sh
+++ 07-special-vars.sh
@@ -5,7 +5,9 @@
 
 echo "the argument passed to the script is:$@"
 echo "the argument passed to the script is:$*"
-
-#it provides file name of script
 echo "the script name is:$0"
-
+echo "the user who is running the script is:$USER"
+echo "current directoty os the script:$PWD"
+echo "home directory of user is:$HOME"
+echo  "process instant id (PID) of the process is:$PID"
+echo "PID of last command in back ground running is:$!"
