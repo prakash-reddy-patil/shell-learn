1aca729f90672c2584580469b6fc9d63bfd7a6bf
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 12:55:41 +0530

    file 6 comitted

--- 06-variables.sh
+++ 06-variables.sh
@@ -8,5 +8,5 @@ sleep 10
 
 END_DATE=$(date +%s)
 #echo "date and time after processser sleep is:$END_DATE"
-Total_Time=$((START_DATE-END_DATE))
+Total_Time=$((END_DATE-START_DATE))
 echo "the total time difference after processer sleep is:$Total_time"
