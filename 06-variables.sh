5269292ccfab36035668e6e658e4d12a4a588e0e
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 13:14:12 +0530

    all changes comitted

--- 06-variables.sh
+++ 06-variables.sh
@@ -8,5 +8,19 @@ sleep 10
 
 END_DATE=$(date +%s)
 #echo "date and time after processser sleep is:$END_DATE"
+
 total_time=$((END_DATE-START_DATE))
 echo "the total time difference after processer sleep is:$total_time"
+
+
+
+#Siva notes
+#START_TIME=$(date +%s)
+
+#sleep 10
+
+#END_TIME=$(date +%s)
+
+#TOTAL_TIME=$(($END_TIME-$START_TIME))
+
+#echo "Script executed in: $TOTAL_TIME Seconds"
---
+++ 07-special-vars.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "the argument passed to the script is:'$@"
+echo "the argument passed to the script is:$*"
