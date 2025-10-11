392f5357924b84d54bfefc82fa52c4814ad45cef
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-11 15:58:08 +0530

    file 8 is comitted

--- 08-data-types.sh
+++ 08-data-types.sh
@@ -10,6 +10,8 @@ sum=$((num1+num2))
 echo "the addition of two numbers is:$sum"
 
 
+
+# Size = 4, Max index = 3
 LEADERS=("modi" "trump" "patil" "putin")
 
 echo "All leaders are present in array is:${LEADERS[@]}"
@@ -17,3 +19,25 @@ echo "All leaders are present in array is:${LEADERS[@]}"
 echo "the first leader in array is:${LEADERS[0]}"
 echo "the third leader in array is:${LEADERS[2]}"
 
+
+
+
+
+
+<<'END_COMMENT'
+# everything in shell is considerd as string
+NUMBER1=100
+NUMBER2=200
+NAME=DevOps
+
+SUM=$(($NUMBER1+$NUMBER2+$NAME))
+
+echo "SUM is: ${SUM}"
+
+# Size = 4, Max index = 3
+LEADERS=("Modi" "Putin" "Trudo" "Trump")
+
+echo "All leaders: ${LEADERS[@]}"
+echo "First Leader: ${LEADERS[0]}"
+echo "First Leader: ${LEADERS[10]}" 
+END_COMMENT 
