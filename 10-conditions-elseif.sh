18b72509f5cd6c81fc16898d00afa5dad4937afa
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-12 17:00:07 +0530

    files are comitted

--- 10-conditions-elseif.sh
+++ 10-conditions-elseif.sh
@@ -2,10 +2,11 @@
 
 number=$1
 
-if [ $number -lt 10 ]
+if [ $number -lt 10 ];
    then
    echo "the number is less than 10"
-elif [ $number -eq 10 ]
+elif [ $number -eq 10 ];
+   then
    echo "the number is equal to 10"
 else 
    echo "the is greater than  10"
