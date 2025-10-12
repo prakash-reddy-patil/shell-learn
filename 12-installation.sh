073dc7ad875b81cec6973c0bd6da7ba9b3b6052a
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-12 19:10:29 +0530

    11th file comitted

---
+++ 12-installation.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $userid -nt 0 ]; then 
+
+    echo "ERROR:: Please run this script with root privelage"
+
+fi
+
+ dnf install nginx -y 
+
+if [ $? -nt 0 ] ; then 
+ 
+    echo "EROR:: installing MYSQl is failure"
+ else 
+    echo "Installing MYSQL is SCUCCES"
+fi
