581ce881df998fdaf06b7f9f6816e0876c840721
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-12 16:21:43 +0530

    09 files are comitted

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -7,7 +7,8 @@ then
    echo "the given number is less than 10"
    else
    echo "the given number is not less than 10"
-   else if 
-   if [ $number -eq 10 ]
-   echo "the given number is equal to 10
+fi
+if [ $number -eq 10 ]
+then 
+echo "the given number is equal to 10"
 fi
