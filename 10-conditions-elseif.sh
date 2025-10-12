63445c51795f964e57559f983ce486feb8b00704
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-12 16:56:33 +0530

    file 10 is comitted

---
+++ 10-conditions-elseif.sh
@@ -0,0 +1,12 @@
+#!bin/bash
+
+number=$1
+
+if [ $number -lt 10 ]
+   then
+   echo "the number is less than 10"
+elif [ $number -eq 10 ]
+   echo "the number is equal to 10"
+else 
+   echo "the is greater than  10"
+fi
