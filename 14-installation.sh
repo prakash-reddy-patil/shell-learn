fbfe85a7c4b96117d4724555bcd58d87de10f042
prakash-reddy-patil <patilprakashreddy@gmail.com>
2025-10-13 15:33:50 +0530

    files are comitted

---
+++ 14-installation.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1   #failures other than zero
+
+fi
+
+
+VALIDATION (){
+
+if [ $? -nt o ] ; then 
+  
+    echo "EROR:: installing $2 is failure"
+    exit 1
+ else 
+    echo "Installing $2 is SCUCCES"
+fi
+}
+
+ 
+
+dnf install mysql -y 
+VALIDATION $1  "MYSQl"
+
+dnf install NGINX  -y 
+VALIDATION $1  "NGINX"
+
+dnf install PYTHON3  -y 
+VALIDATION $1  "PYTHON"
+
+
